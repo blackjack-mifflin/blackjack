e1b9400cbcdfbcef892e38c69b92d177b8f285d1
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-09-07 11:36:41 -0700

    initilizeBlackjackGame

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const path = require('path')
+const morgan = require('morgan')
+
+app.use(morgan('dev'));
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')))
+
+app.get('/', (req, res ) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html' ))
+})
+
+app.listen(process.env.PORT, (error) => {
+    if(!error){
+        console.log(`Server is listening on ${process.env.PORT}`);
+    } else {
+        console.log('Not Working')
+    }
+})
