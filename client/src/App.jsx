a471cc4252e73f7ffbd5465c36b4b38e24f41bec
RobertD90 <doroghazi.robert@gmail.com>
2023-09-08 14:15:35 -0600

    NavBar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,24 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Welcome from './components/Welcome';
 import LogInPage from './components/LoginPage'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import SignUpPage from './components/SignUpPage';
 
+
+import Navbar from './components/NavBar';
 
 const App = () => {
 
   return (
     <>
-     <Link className='Nav' to='/Login'>Log In</Link>
-     <Routes>
+      <Link className='Nav' to='/Login'>Log In</Link>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LogIn />}></Route>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<LogInPage />}></Route>
         <Route path="/SignUp" element={<SignUpPage />}></Route>
-    </Routes>
+      </Routes>
 
     </>
   )
