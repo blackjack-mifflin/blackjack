c0ec9ed7eac361153c71ec7807274fb6596381e3
RobertD90 <doroghazi.robert@gmail.com>
2023-09-08 14:06:07 -0600

    Home,Profile,Multiplater,SignOut Buttons Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import LogIn from './components/LoginPage'
 import Welcome from './components/Welcome';
+import Navbar from './components/NavBar';
 
 const App = () => {
 
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<LogIn />}></Route>
         <Route path="/" element={<Welcome />} />
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,16 +2,11 @@ import React from "react";
 const Welcome = () => {
 
 
-    const navigateToProfile = () => {
-        // Handle navigation to the profile page when it is ready(soon)
-        console.log("Navigate to the profile page");
-    };
-
     return (
         <div className="welcome-container">
             <h1>Welcome to Blackjack Mifflin</h1>
             <p>Get ready for the fun!</p>
-            <button onClick={navigateToProfile}>Profile Page</button>
+            {/* <button onClick={navigateToProfile}>Profile Page</button> */}
         </div>
     );
 };
