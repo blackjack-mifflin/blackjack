c3d6119426494e14524c462e7cee6f1622a023ed
RobertD90 <doroghazi.robert@gmail.com>
2023-09-11 13:30:00 -0600

    LastHand Button

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,10 +1,28 @@
-const Game = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Games = () => {
   return (
     <>
       <h1>Blackjack Mifflin</h1>
-      
     </>
-  )
-}
+  );
+};
 
-export default Game;
+const multiplayer = () => {
+
+  const exitGame = () => {
+  };
+
+  return (
+    <div>
+      <h1>Multiplayer Game</h1>
+      <div>
+
+        <button onClick={exitGame}>Last Hand</button>
+      </div>
+    </div>
+  );
+};
+
+export default Games;
