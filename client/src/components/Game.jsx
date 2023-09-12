30ea97b93a67a19f7efdb9afc359e23c91adbd1c
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-09-12 09:39:49 -0600

    deleted unneeded code from Game

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -22,35 +22,6 @@ const Game = () => {
   socket.on("cards", (card) => {
     console.log(card);
   });
+}
 
-import React from "react";
-import { Link } from "react-router-dom";
-
-const Games = () => {
-  return (
-    <>
-      <h1>Blackjack Mifflin</h1>
-      <button onClick={lastHand}>Last Hand</button>
-      <button onClick={socketHandler} value="hit">Hit</button>
-      <button onClick={socketHandler} value="stick">Stick</button>
-    </>
-  );
-};
-
-const multiplayer = () => {
-
-  const exitGame = () => {
-  };
-
-  return (
-    <div>
-      <h1>Multiplayer Game</h1>
-      <div>
-
-        <button onClick={exitGame}>Last Hand</button>
-      </div>
-    </div>
-  );
-};
-
-export default Games;
+export default Game;
