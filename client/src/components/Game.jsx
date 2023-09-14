27c1fd92aa7f480ee43d4b9c3c26ffd2464daaa7
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-09-14 12:36:55 -0600

    added player stats to gamee

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
+import PlayerStats from "./PlayerStats";
 
 const Game = () => {
   const [isLastHand, setIsLastHand] = useState(false);
   const navigate = useNavigate();
-  const socket = io('http://localhost:3000');
+  const socket = io("http://localhost:3000");
   const [betSize, setBetSize] = useState(0);
   const [currentHandBet, setCurrentHandBet] = useState(0);
 
@@ -18,13 +19,13 @@ const Game = () => {
   const socketHandler = (e) => {
     const message = e.target.value;
     console.log(`MOVE FROM CLIENT: ${message}`);
-    socket.emit('move', message);
+    socket.emit("move", message);
   };
 
-  socket.on('card', (card) => {
+  socket.on("card", (card) => {
     console.log(`CARD FROM SERVER: ${card}`);
   });
-  socket.on('player', (playerIdx) => {
+  socket.on("player", (playerIdx) => {
     console.log(`Current player at seat ${playerIdx}`);
   });
 
@@ -33,17 +34,18 @@ const Game = () => {
     setCurrentHandBet(betSize);
     const id = 1;
     const response = await fetch(`/api/players/bet/${id}`, {
-      method: 'PUT',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({"balance": 0-betSize})
-    })
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ balance: 0 - betSize }),
+    });
     const result = await response.json();
     console.log(result);
-  }
+  };
 
   return (
     <>
       <h1>Blackjack Mifflin</h1>
+      <PlayerStats currentHandBet={currentHandBet} />
 
       <button onClick={lastHand}>Last Hand</button>
 
@@ -56,7 +58,11 @@ const Game = () => {
       </button>
       <h3>Bet Size: {currentHandBet}</h3>
       <form onSubmit={betHandler}>
-        <input type="number" min="0" onChange={(e) => setBetSize(e.target.value)}/>
+        <input
+          type="number"
+          min="0"
+          onChange={(e) => setBetSize(e.target.value)}
+        />
         <button>Bet</button>
       </form>
       <div style={{ textAlign: "center", marginBottom: "20px" }}>
@@ -71,4 +77,3 @@ const Game = () => {
 };
 
 export default Game;
-
