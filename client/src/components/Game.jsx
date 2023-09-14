f5f72f6d11bf0eb39c393b1b2dd593c2c671388b
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-09-14 10:07:29 -0600

    frontend bets connect with backend

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -6,6 +6,8 @@ const Game = () => {
   const [isLastHand, setIsLastHand] = useState(false);
   const navigate = useNavigate();
   const socket = io('http://localhost:3000');
+  const [betSize, setBetSize] = useState(0);
+  const [currentHandBet, setCurrentHandBet] = useState(0);
 
   const lastHand = () => {
     console.log("move to home page");
@@ -26,6 +28,19 @@ const Game = () => {
     console.log(`Current player at seat ${playerIdx}`);
   });
 
+  const betHandler = async (e) => {
+    e.preventDefault();
+    setCurrentHandBet(betSize);
+    const id = 1;
+    const response = await fetch(`/api/players/bet/${id}`, {
+      method: 'PUT',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({"balance": 0-betSize})
+    })
+    const result = await response.json();
+    console.log(result);
+  }
+
   return (
     <>
       <h1>Blackjack Mifflin</h1>
@@ -39,6 +54,11 @@ const Game = () => {
       <button onClick={socketHandler} value="stick">
         Stick
       </button>
+      <h3>Bet Size: {currentHandBet}</h3>
+      <form onSubmit={betHandler}>
+        <input type="number" min="0" onChange={(e) => setBetSize(e.target.value)}/>
+        <button>Bet</button>
+      </form>
       <div style={{ textAlign: "center", marginBottom: "20px" }}>
         <img
           src="https://i0.wp.com/mediachomp.com/wp-content/uploads/2022/02/the-office-cartoon-characters-15.jpg?resize=500%2C707&ssl=1"
