d02d7b42173a136c66516b290a0db18476c7deda
RobertD90 <doroghazi.robert@gmail.com>
2023-09-15 14:49:10 -0600

    Messages Name Fix

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,46 +1,10 @@
-// import React, { useState, useEffect } from 'react';
-// import { io } from 'socket.io-client';
-
-// const Welcome = () => {
-//     const [messages, setMessages] = useState([]);
-//     const [inputMessage, setInputMessage] = useState('');
-//     const socket = io('/');
-
-//     socket.on('new message', (data) => {
-//         setMessages((prevMessages) => [...prevMessages, data]);
-//     });
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         socket.emit('message', { name: 'YourName', message: inputMessage });
-//         setInputMessage('');
-//     };
-
-//     return (
-//         <div className="welcome-container">
-//             <h1>Welcome to Blackjack Mifflin</h1>
-//             <form onSubmit={handleSubmit} className="message-form">
-//                 <input
-//                     type="text"
-//                     value={inputMessage}
-//                     onChange={(e) => setInputMessage(e.target.value)}
-//                     className="message-input"
-//                 />
-//                 <button type="submit" className="message-button">Send</button>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default Welcome;
-
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
 const Welcome = () => {
     const [messages, setMessages] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
-    const [inputName, setInputName] = useState(''); // Add a new state for the user's name
+    const [inputName, setInputName] = useState('');
     const socket = io('/');
 
     socket.on('new message', (msg) => {
@@ -50,7 +14,7 @@ const Welcome = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Send the message and user's name to the server
+
         socket.emit('message', { name: inputName, message: inputMessage });
 
         setInputMessage('');
@@ -60,7 +24,7 @@ const Welcome = () => {
         <div className="welcome-container">
             <h1>Welcome to Blackjack Mifflin</h1>
             <form onSubmit={handleSubmit} className="message-form">
-                {/* Input field for the user's name */}
+
                 <input
                     type="text"
                     placeholder="Your Name"
@@ -68,7 +32,7 @@ const Welcome = () => {
                     onChange={(e) => setInputName(e.target.value)}
                     className="name-input"
                 />
-                {/* Input field for the message */}
+
                 <input
                     type="text"
                     placeholder="Your Message"
--- server/index.js
+++ server/index.js
@@ -1,41 +1,3 @@
-// const express = require('express');
-// const app = express();
-// require('dotenv').config()
-// const path = require('path')
-// const morgan = require('morgan');
-// app.use(require("body-parser").json());
-// const jwt = require("jsonwebtoken");
-
-// app.use(morgan('dev'));
-// app.use(require("body-parser").json());
-
-// app.use((req, res, next) => {
-//     const auth = req.headers.authorization;
-//     const token = auth?.startsWith("Bearer") ? auth.slice(7) : null;
-//     try {
-//       const { id } = jwt.verify(token, process.env.JWT);
-//       req.userID = id;
-//     } catch (err) {
-//       req.userID = null;
-//     }
-//     next();
-//   });
-// app.use(express.static(path.join(__dirname, '../client/dist')))
-
-// app.get('/', (req, res ) => {
-//     res.sendFile(path.join(__dirname, '../client/dist/index.html' ))
-// })
-
-// app.use('/api', require('./api'));
-// app.use('/auth', require('./auth'));
-
-// app.listen(process.env.PORT, (error) => {
-//     if(!error){
-//         console.log(`Server is listening on ${process.env.PORT}`);
-//     } else {
-//         console.log('Not Working')
-//     }
-// })
 
 const express = require('express');
 const app = express();
@@ -78,10 +40,10 @@ io.on('connection', (socket) => {
     console.log(`User disconnected! Socket ID: ${socket.id}`);
   });
 
-  // Handle messages from the client
+
   socket.on('message', (data) => {
-    const { name, message } = data; // Extract name and message from data
-    io.emit('new message', { name, message }); // Emit both name and message
+    const { name, message } = data;
+    io.emit('new message', { name, message });
   });
 });
 
