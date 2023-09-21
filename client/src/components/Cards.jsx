41c591e7bc4c0c6cc622fc375be042833a535456
RobertD90 <doroghazi.robert@gmail.com>
2023-09-21 12:02:41 -0600

    first commit

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,20 +1,45 @@
+// const Cards = () => {
+//   const playerIds = [1, 2];
+//   class PlayerHand {
+//     construct() {
+//       this.username
+//       this.cards
+//     }
+//   }
+
+//   return (
+//     <>
+//       <h1>Cards</h1>
+//       <div id="dealer">
+
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Cards;
+
+import React, { useState } from 'react';
+
 const Cards = () => {
-  const playerIds = [1, 2];
-  class PlayerHand {
-    construct() {
-      this.username
-      this.cards
-    }
-  }
+  const [playerHand, setPlayerHand] = useState([]);
+
+  const addCardToHand = (card) => {
+    setPlayerHand([...playerHand, card]);
+  };
 
   return (
     <>
-      <h1>Cards</h1>
+      <h1>Player's Hand</h1>
       <div id="dealer">
-
+        <div className="player-hand">
+          {playerHand.map((card, index) => (
+            <Card key={index} cardImage={card} />
+          ))}
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Cards;
